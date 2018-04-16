068242319463aa88801f2285618311cc08108714
RobPiwowarek <robpiwowarek@gmail.com>
2018-04-16 23:40:06 +0200

    implement probably not working version of insert and query for mariadb MBT: #0000009

--- src/main/scala/server/database/MariaDb.scala
+++ src/main/scala/server/database/MariaDb.scala
@@ -1,14 +1,50 @@
 package server.database
 
 import com.typesafe.config.ConfigFactory
+import domain.Domain.{Password, Username}
 import slick.jdbc.MySQLProfile.api._
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
-object MariaDb
-{
-  val config = ConfigFactory.parseResources("application.conf")
+object MariaDb {
+  val db = Database.forConfig("mariadb", ConfigFactory.parseResources("application.conf"))
 
-  val db = Database.forConfig("mariadb")
+  val setup = DBIO.seq()
+
+  // tables:
+
+  class Users(tag: Tag) extends Table[(String, String)](tag, "USERS") {
+    def login = column[String]("LOGIN", O.PrimaryKey)
+
+    def password = column[String]("PASSWORD")
+
+    def * = (login, password)
+  }
+
+  val users = TableQuery[Users]
+
+  def insert(username: Username, password: Password) = {
+    try {
+      Await.result(
+        db.run(
+          DBIO.seq(
+            users ++= Seq(
+              (username.name, password.password)))),
+        Duration.Inf) //todo: inny duration
+    } finally db.close
+  }
+
+  def query(username: Username, password: Password) = {
+    db.run(
+      users.result)
+      .map(_.foreach {
+        case (login, pass) =>
+          if (login.equals(username.name) && pass.equals(password.password))
+            println("query successful")
+        case _ =>
+          println("not found")
+      })
+  }
 
 }
