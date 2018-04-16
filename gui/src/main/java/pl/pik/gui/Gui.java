6d1cfa8e9a76989c4f5abbbfdfdb75ec98f3b4c9
Jakub M. <J.Maciazek@stud.elka.pw.edu.pl>
2018-04-16 13:47:32 +0200

    Production mode - running angular app on spring-boot;

---
+++ gui/src/main/java/pl/pik/gui/Gui.java
@@ -0,0 +1,12 @@
+package pl.pik.gui;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gui {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gui.class, args);
+	}
+
+}
