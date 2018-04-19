0da0c290c4f865b9dc2e4990367dc4ac8a3f7486
agatak8 <shnutzer@gmail.com>
2018-04-19 19:30:44 +0200

    move properties and authenticator out of createMessage in Email

--- src/main/scala/notifications/Email.scala
+++ src/main/scala/notifications/Email.scala
@@ -7,7 +7,7 @@ import akka.actor.Actor
 import com.typesafe.config._
 import javax.mail._
 import javax.mail.internet.{InternetAddress, MimeMessage}
-import notifications.Errors.{AuthorizationError, TimeoutError, InternalError, SendError}
+import notifications.Errors.{AuthorizationError, InternalError, SendError, TimeoutError}
 
 sealed trait EmailConfig {
   val login: String
@@ -19,8 +19,8 @@ sealed trait EmailConfig {
 }
 
 object AutoFileConfig extends EmailConfig {
-  // change config in src/main/resources/reference.conf
-  // or in src/main/resources/application.conf (will override reference.conf)
+  // copy src/main/resources/reference.conf to src/main/resources/application.conf
+  // and set values manually (it will override reference.conf)
   val conf = ConfigFactory.load()
   val login = conf.getString("email.login")
   val from = login
@@ -31,21 +31,21 @@ object AutoFileConfig extends EmailConfig {
 }
 
 class Email(conf: EmailConfig) extends Actor {
+  val properties = new Properties()
+  properties.put("mail.transport.protocol", "smtp")
+  properties.put("mail.smtp.host", conf.host)
+  properties.put("mail.smtp.port", conf.port)
+  properties.put("mail.smtp.auth", "true")
+  properties.put("mail.smtp.starttls.enable", "true")
+  properties.put("mail.smtp.connectiontimeout", conf.timeout)
+  properties.put("mail.smtp.timeout", conf.timeout)
+
+  val authenticator = new Authenticator() {
+    override def getPasswordAuthentication = new
+        PasswordAuthentication(conf.login, conf.password)
+  }
+
   def createMessage(to: String, subject: String, content: String): Message = {
-    val properties = new Properties()
-    properties.put("mail.transport.protocol", "smtp")
-    properties.put("mail.smtp.host", conf.host)
-    properties.put("mail.smtp.port", conf.port)
-    properties.put("mail.smtp.auth", "true")
-    properties.put("mail.smtp.starttls.enable", "true")
-    properties.put("mail.smtp.connectiontimeout", conf.timeout)
-    properties.put("mail.smtp.timeout", conf.timeout)
-
-    val authenticator = new Authenticator() {
-      override def getPasswordAuthentication = new
-          PasswordAuthentication(conf.login, conf.password)
-    }
-
     val session = Session.getInstance(properties, authenticator)
     val message = new MimeMessage(session)
     message.setFrom(new InternetAddress(conf.from))
