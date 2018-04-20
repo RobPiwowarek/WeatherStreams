fc663e2d829e755e97f0049d65831b3eae3dfef4
RobPiwowarek <robpiwowarek@gmail.com>
2018-04-20 10:02:04 +0200

    starting work on owm integration

---
+++ src/main/scala/providers/openweathermap/OpenWeatherMapClient.scala
@@ -0,0 +1,9 @@
+package providers.openweathermap
+
+class OpenWeatherMapClient {
+  implicit val apiKey = ApiKey("84ab538642f8529e836220e3232f7940")
+
+  val endpoint_base = "api.openweathermap.org/data/2.5/"
+
+
+}
--- src/main/scala/providers/openweathermap/Requests.scala
+++ src/main/scala/providers/openweathermap/Requests.scala
@@ -1,5 +1,13 @@
 package providers.openweathermap
 
 object Requests {
+  sealed trait OpenWeatherMapRequest {
+    def query(implicit apiKey: ApiKey)
+  }
+
+
+
 
 }
+
+final case class ApiKey(value: String)
