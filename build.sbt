18895973aeed6f5ce3596d5d4fe83cff9fa52fa8
RobPiwowarek <robpiwowarek@gmail.com>
2018-04-20 09:27:48 +0200

    slick helloworld MBT: #0000009

--- build.sbt
+++ build.sbt
@@ -15,7 +15,7 @@ libraryDependencies ++= Seq(
   "org.slf4j"           % "slf4j-nop"      % "1.6.4", // needed for slick
   "com.typesafe.slick" %% "slick-hikaricp" % "3.2.3", // not necessarily needed for slick
 
-  "com.lightbend.akka" %% "akka-stream-alpakka-slick" % "0.17",
+  "com.lightbend.akka" %% "akka-stream-alpakka-slick" % "0.18",
 
   "org.scalatest"          %% "scalatest"          % "3.2.0-SNAP10"    % Test,
   "com.github.tomakehurst" % "wiremock-standalone" % "2.15.0"          % Test,
--- src/main/scala/server/database/MariaDb.scala
+++ src/main/scala/server/database/MariaDb.scala
@@ -1,50 +1,14 @@
 package server.database
 
-import com.typesafe.config.ConfigFactory
-import domain.Domain.{Password, Username}
-import slick.jdbc.MySQLProfile.api._
-
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+import akka.stream.alpakka.slick.javadsl.SlickSession
+import slick.basic.DatabaseConfig
+import slick.jdbc.JdbcProfile
 
 object MariaDb {
-  val db = Database.forConfig("mariadb", ConfigFactory.parseResources("application.conf"))
 
-  val setup = DBIO.seq()
+  val databaseConfig = DatabaseConfig.forConfig[JdbcProfile]("slick-mysql")
+  implicit val session = SlickSession.forConfig(databaseConfig)
 
-  // tables:
-
-  class Users(tag: Tag) extends Table[(String, String)](tag, "USERS") {
-    def login = column[String]("LOGIN", O.PrimaryKey)
-
-    def password = column[String]("PASSWORD")
-
-    def * = (login, password)
-  }
-
-  val users = TableQuery[Users]
-
-  def insert(username: Username, password: Password) = {
-    try {
-      Await.result(
-        db.run(
-          DBIO.seq(
-            users ++= Seq(
-              (username.name, password.password)))),
-        Duration.Inf) //todo: inny duration
-    } finally db.close
-  }
-
-  def query(username: Username, password: Password) = {
-    db.run(
-      users.result)
-      .map(_.foreach {
-        case (login, pass) =>
-          if (login.equals(username.name) && pass.equals(password.password))
-            println("query successful")
-        case _ =>
-          println("not found")
-      })
-  }
+  
 
 }
