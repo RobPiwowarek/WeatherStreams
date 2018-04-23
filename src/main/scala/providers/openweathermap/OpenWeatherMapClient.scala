ab4c58df19cb28ad8124822f68b6698c7ca7f6ed
RobPiwowarek <robpiwowarek@gmail.com>
2018-04-23 21:21:40 +0200

    change url MBT: #0000009

--- src/main/scala/providers/openweathermap/OpenWeatherMapClient.scala
+++ src/main/scala/providers/openweathermap/OpenWeatherMapClient.scala
@@ -8,14 +8,14 @@ import providers.WeatherClient
 object OpenWeatherMapClient extends WeatherClient{
   implicit val apiKey = ApiKey("84ab538642f8529e836220e3232f7940")
 
-  val endpointBase = Uri.parse("api.openweathermap.org/data/2.5/")
+  val endpointBase = Uri.parse("api.openweathermap.org/data/2.5")
   val weatherEndpoint = endpointBase / "weather?"
   val forecastEndpoint = endpointBase / "forecast?"
 
   val httpClient = asyncHttpClient()
 
   def getWeatherData(params: Seq[(String, String)]) = {
-    val whenResponse = httpClient.prepareGet(weatherEndpoint.addParams(params) & ("appid", apiKey)).execute()
+    val whenResponse = httpClient.prepareGet(weatherEndpoint.addParams(params) & ("appid", apiKey.value)).execute()
     whenResponse.get
   }
 
