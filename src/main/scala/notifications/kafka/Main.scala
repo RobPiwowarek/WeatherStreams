1c3491ac3f90ad7ceb7fef33c7d410bc074beb55
agatak8 <shnutzer@gmail.com>
2018-06-03 23:22:32 +0200

    add Producer and Stream threads to kafka Main

--- src/main/scala/notifications/kafka/Main.scala
+++ src/main/scala/notifications/kafka/Main.scala
@@ -2,9 +2,14 @@ package notifications.kafka
 
 import java.util.concurrent.Executors
 
+import server.database.MariaDb
+
 object Main extends App {
-  val pool = Executors.newFixedThreadPool(2)
+  val mariaDb = new MariaDb()
+  val pool = Executors.newFixedThreadPool(4)
   pool.submit(Consumers.Email)
   pool.submit(Consumers.Slack)
+  pool.submit(new Producer(mariaDb))
+  pool.submit(new Stream(mariaDb))
 }
 
